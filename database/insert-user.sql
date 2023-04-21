e791979bdb5857ec7ce2f5c8c3af54921e674741
Jovan Dosen <jovandosen1994@gmail.com>
2023-04-21 19:25:01 +0200

    SQL added

---
+++ database/insert-user.sql
@@ -0,0 +1,4 @@
+INSERT INTO Users
+(FirstName, LastName, Email, Country, City, Created, Updated)
+VALUES
+('John', 'Doe', 'john@gmail.com', 'Mexico', 'México D.F', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);

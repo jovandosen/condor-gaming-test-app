d8386bf708332f4604a01e732b2137b34b95fde4
Jovan Dosen <jovandosen1994@gmail.com>
2023-04-22 12:26:49 +0200

    Example of constructor property promotion added

--- app/Mvc/Models/Model.php
+++ app/Mvc/Models/Model.php
@@ -2,10 +2,12 @@
 
 namespace App\Mvc\Models;
 
+// example of constructor property promotion 
 class Model
 {
-    public function __construct()
-    {
-        echo 'First model';
-    }
+    public function __construct(
+        public string $name,
+        public int $age,
+        public ?string $email = null
+    ) {}
 }
--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Homepage';
+use App\Mvc\Models\Model;
+ 
+// Creating a new Model object
+$model = new Model("Alice", 30, "alice@example.com");
+
+// Accessing object properties
+echo "Name: " . $model->name;
+echo "Age: " . $model->age;
+echo "Email: " . $model->email;
